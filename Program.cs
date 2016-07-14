3546bf0d9d4b6937e5a770640ce5cd7b33a67a48
XuongKho <nqhung139@gmail.com>
2016-07-14 23:21:37 +0700

    create project

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnetcoreapp
+{
+    public class Program
+    {
+        //ech git dau mat roi 
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
